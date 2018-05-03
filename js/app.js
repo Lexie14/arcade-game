78ad56f8a55504395d85404c01cc1155826e0249
Lexie <oleksandrapas@gmail.com>
2018-05-03 22:36:41 +0200

    - freezeEnemies function; - resetPlayer function; - restartGame function; - add/remove EventListener to keyboard actions (move(stop moving) the player); - actions taken after Restart button clicked (both won and lost case); - comments added; - code syntax revised;

--- js/app.js
+++ js/app.js
@@ -1,45 +1,42 @@
-// Enemies our player must avoid
+// Enemy class
 let Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = (Math.random() * 100) + 10;
+    this.speed = (Math.random() * 150) + 10;
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
-const lives = document.querySelector('.lives');
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += dt * this.speed;
     if (this.x >= 505) {
         this.x = -50;
     }
+    // Handle collision between the player and enemies
     if (this.x < player.x + 75 && this.x + 75 > player.x && this.y < player.y + 70 && this.y + 70 > player.y) {
-         player.x = 200;
-         player.y = 400;
+        resetPlayer();
         $('.lives :last-child').remove();
+        // Actions taken when the plays lost all lives (game is lost)
+        const lives = document.querySelector('.lives');
         if (lives.childElementCount === 0) {
-          displayLost();
+            freezeEnemies();
+            displayLost();
+            document.removeEventListener('keyup', playerMove);
         }
-   }
+    }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
+// Player's constructor function
 let Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
@@ -47,21 +44,25 @@ let Player = function(x, y) {
 };
 
 Player.prototype.update = function() {
+    // Prevent the player from moving outside the canvas
     if (this.x > 404) this.x = 404;
     if (this.x < 0) this.x = 0;
     if (this.y > 400) this.y = 400;
     if (this.y < -20) this.y = 400;
-    if (this.y < 0) {
-      this.x = 200;
-      this.y = 400;
-      displayWon();
+    // Actions taken when the player reaches water (game is won)
+    if (this.y < 20) {
+        resetPlayer();
+        freezeEnemies();
+        displayWon();
     }
 };
 
+// Draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Move the player via keyboard's arrows
 Player.prototype.handleInput = function(allowedKeys) {
     var x = event.keyCode;
     if (x == 37) {
@@ -75,9 +76,8 @@ Player.prototype.handleInput = function(allowedKeys) {
     }
     player.update();
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+// Instantiate all objects
 const allEnemies = [
     new Enemy(-50, 60),
     new Enemy(-50, 120),
@@ -87,24 +87,69 @@ const allEnemies = [
 
 const player = new Player(200, 400);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// This listens for key presses and sends the keys
+// Player.handleInput() method
+const playerMove = function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
+}
+
+document.addEventListener('keyup', playerMove);
+
+// Bring the player to the initial position
+function resetPlayer() {
+    player.x = 200;
+    player.y = 400;
+}
+
+// Freeze all enemies' movement
+function freezeEnemies() {
+    allEnemies.forEach(function(enemy) {
+        enemy.speed = 0;
+    })
+}
+
+// Display modal when the player lost
+const lost = document.querySelector('.lost');
+
+function displayLost() {
+    lost.style.display = 'block';
+}
+
+// Actions taken when the Restart button is clicked on the modal (lost case)
+const lostRestart = document.querySelector('.lostRestart');
+
+lostRestart.addEventListener('click', function() {
+    lost.style.display = 'none';
+    resetPlayer();
+    restartGame();
 });
 
+// Display modal when the player won
 const won = document.querySelector('.won');
+
 function displayWon() {
-  won.style.display = 'block';
+    won.style.display = 'block';
 }
-const lost = document.querySelector('.lost');
-function displayLost() {
-  lost.style.display = 'block';
+
+// Actions taken when the Restart button is clicked on the modal (won case)
+const wonRestart = document.querySelector('.wonRestart');
+
+wonRestart.addEventListener('click', function() {
+    won.style.display = 'none';
+    resetPlayer();
+    restartGame();
+});
+
+// Restart number of lives and reload the game
+const heart = '<img src="images/Heart.png" alt="heart" height="5%" width="5%"> \n';
+
+function restartGame() {
+    lives.innerHTML = heart.repeat(3);
+    location.reload();
 }
